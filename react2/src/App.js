2cdfe62a68eeb6bb7dae890fb9684287763d4264
idealful <idealful@gmail.com>
2023-03-03 19:43:17 +0900

    React 2022년 개정판 - 7. state

--- react2/src/App.js
+++ react2/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import { useState } from 'react';
 
 function Article(props) {
   return (
@@ -39,7 +39,7 @@ function Nav(props) {
           href={'/read/' + t.id}
           onClick={(event) => {
             event.preventDefault();
-            props.onChangeMode(event.target.id);
+            props.onChangeMode(Number(event.target.id));
           }}
         >
           {t.title}
@@ -56,27 +56,53 @@ function Nav(props) {
 }
 
 function App() {
+  // let _mode = useState('WELCOME');
+  // const mode = _mode[0];
+  // const setState = _mode[1];
+  // console.log('_mode', _mode);
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
+
   const topics = [
     { id: 1, title: 'html', body: 'html is...' },
     { id: 2, title: 'css', body: 'css is...' },
     { id: 3, title: 'javascript', body: 'javascript is...' },
   ];
 
+  let content = null;
+  if (mode === 'WELCOME') {
+    content = <Article title='Welcome' body='Hello, WEB'></Article>;
+  } else if (mode === 'READ') {
+    let title,
+      body = null;
+
+    for (let i = 0; i < topics.length; i++) {
+      console.log(topics[i].id, id);
+      if (topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+
+    content = <Article title={title} body={body}></Article>;
+  }
+
   return (
     <div>
       <Header
         title='WEB'
         onChangeMode={() => {
-          alert('Header');
+          setMode('WELCOME');
         }}
       ></Header>
       <Nav
         topics={topics}
-        onChangeMode={(id) => {
-          alert(id);
+        onChangeMode={(_id) => {
+          setMode('READ');
+          setId(_id);
         }}
       ></Nav>
-      <Article title='Welcome' body='Hello, WEB'></Article>
+      {content}
     </div>
   );
 }
